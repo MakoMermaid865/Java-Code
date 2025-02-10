83646f70c500ad568fc7a51b564e7f45a0db3a5c
Ari Kinney <akinney424@gmail.com>
2025-02-10 15:09:12 -0500

    Create Detector.java

---
+++ Birthday/Detector.java
@@ -0,0 +1,6 @@
+//this will utilize a calender and ask for the user birthday and on the day it was inputed will say its your birthday and give a prize
+//if not birthday then end program saying it it not your birthday but it is coming up in certain days and able to do a countdown
+ public class Detector{
+   
+ }
+}

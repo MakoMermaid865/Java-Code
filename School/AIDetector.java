d9d1b8b58802822e27bd9a79c2e4b6c33d626548
Ari Kinney <akinney424@gmail.com>
2025-02-10 15:23:48 -0500

    Create AIDetector.java

---
+++ School/AIDetector.java
@@ -0,0 +1,4 @@
+//I was wrongfully accuses of using ai in my code regardless of my notes i used to prove my scienario was overlook and misjudede
+//I wonder how the code dector would work this is my version on a dector I would use on students work
+//allows you to see what is ai and the student work if copypasted able to show from what site and the work process shown visually
+//Ideally of there is copy paste it depends on what platforms that makes it Ai

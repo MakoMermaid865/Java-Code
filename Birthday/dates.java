1606947ad3d1e0f136e00f254b92ab785f189e13
Ari Kinney <akinney424@gmail.com>
2025-02-10 15:01:11 -0500

    Update dates.java

--- Birthday/dates.java
+++ Birthday/dates.java
@@ -2,26 +2,10 @@ import java.util.Scanner;
 public class dates {
     //This is the page that I used understand the using the primitive data types include
     //https://www.geeksforgeeks.org/data-types-in-java/
-
+//this is a shorter version of the birthday code really only needing to know the date year and birth month, like how it is when applying to job or online apps
     public static void main(String[] args) {
         Scanner scnr = new Scanner (System.in);
 
-        System.out.println("What time were you born...yes this is in military time: Example is 10.15 or 20.33");
-        double time = scnr.nextDouble();
-        System.out.println("Time: " + time );
-        scnr.nextLine(); //this will clear the screen and go to a new line for better style
-
-
-        //this was a trick one because I couldn't find the scanner for a char variable Ai (chat.pgt was used to version (mostly the second line) I asked it to fix my error and this was the result
-//        System.out.println("What is the first letter your month starts with");
-//        char letter = scnr.nextline();
-//        System.out.println("First letter of Month: " + letter );
-        System.out.println("What is the first letter that your month starts with");
-        char letter = scnr.next().charAt(0); // this work is documented in the notes sent later on explain through Ai
-        System.out.println("First letter of Month: " + letter );
-        scnr.nextLine();
-
-        //back to geeks website layout lol
         System.out.println("What is the month you were born?");
         String month = scnr.nextLine();
         System.out.println("Month: " + month );
@@ -37,17 +21,7 @@ public class dates {
         System.out.println("Day: " + year );
         scnr.nextLine();
 
-
-        //how do Java booleans work... this can help https://www.w3schools.com/java/java_booleans.asp
-        //notes are in book btw
-        System.out.println("Your birthday is not a holiday: True of False? ");
-        //camelCase is used of java!
-        boolean notHoliday = scnr.nextBoolean();
-        System.out.println("Holiday: " + notHoliday);
-        scnr.nextLine();
-
-
-        System.out.println("Great you were born at " + month + " " + date + " " + year + " "  + "at: "+ time + " So, the letter that you were born is "  + letter + " and it is " + notHoliday + " for being a holiday, Thank you!");
+        System.out.println("Great you were born at " + month + " " + date + " " + year + "!");
 
         scnr.close();//make sure scanner is closed it is not mandatory like you will not get a error if it is not there but its good syntax
 

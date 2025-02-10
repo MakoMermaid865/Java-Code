e3aba62e957063c5e6f544731358f941b5f85372
Ari Kinney <akinney424@gmail.com>
2025-02-10 15:14:45 -0500

    Create StudyHabitTracker.java

---
+++ School/StudyHabitTracker.java
@@ -0,0 +1,2 @@
+//this is a study habit tracker that will track the user studing and how they did on assigments and give a full report on that user
+// Regardless if user did good or bad then it will give tips on how to imporve and ways to study for different subjects...this is research so not all is accurate but i will site my sorces

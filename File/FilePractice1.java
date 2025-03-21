8d50d65a4e46b8eccaa1c919bc563401b02fb29f
Ari Kinney <akinney424@gmail.com>
2025-03-20 21:32:54 -0400

    Add files via upload

---
+++ File/FilePractice1.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class FilePractice1 {
+    public static void main(String[] args)  throws Exception {
+        Scanner txtScanner = new Scanner (System.in);
+        double [] fiveNums;
+        double sum= 0; 
+        File reader = new File("num.txt");
+        while ("num.txt"){
+
+        }
+ 
+        
+    }
+}
